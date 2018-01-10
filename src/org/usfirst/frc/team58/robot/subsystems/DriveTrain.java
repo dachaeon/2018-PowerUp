f075a167024eb4213512d6c623262f1770dce965
Tyler Hansen <TyHansen00@gmail.com>
2018-01-09 19:25:49 -0500

    Update DriveTrain - Tyler

--- src/org/usfirst/frc/team58/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team58/robot/subsystems/DriveTrain.java
@@ -13,14 +13,20 @@ public class DriveTrain extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	
-	TalonSRX m_FrontRightMotor;
-	TalonSRX m_FrontLeftMotor;
-	Talon m_RightSlave;
-	Talon m_LeftSlave;
+	private WPI_TalonSRX m_FrontRightMotor;
+	private WPI_TalonSRX m_FrontLeftMotor;
+	private WPI_TalonSRX m_RightSlave;
+	private WPI_TalonSRX m_LeftSlave;
 	
 	
 	public DriveTrain() {
+		m_FrontRightMotor = new WPI_TalonSRX(3); //numbers to be added once we know what is on CANbus - Tyler
+		m_FrontLeftMotor = new WPI_TalonSRX(4);
+		m_RightSlave = new WPI_TalonSRX(5);
+		m_LeftSlave = new WPI_TalonSRX(6);
 		
+		m_RightSlave.set(com.ctre.pheonix.MotorControl.ControlMode.Follower, 3);
+		m_LeftSlave.set(com.ctre.pheonix.MotorControl.ControlMode.Follower, 4);
 	}
 
 	public void initDefaultCommand() {
