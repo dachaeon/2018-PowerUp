7a955dadb82386ea5ca066247b233048f48848a3
Tyler Hansen <TyHansen00@gmail.com>
2018-01-13 18:59:59 -0500

    minor changes i didnt commit
    just commiting this to avoid merge errors

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 2018-PowerUp
+Team 58's 2018 Code.
--- src/org/usfirst/frc/team58/robot/commands/MiddleSwitch.java
+++ src/org/usfirst/frc/team58/robot/commands/MiddleSwitch.java
@@ -8,13 +8,13 @@ public class MiddleSwitch extends CommandGroup {
 	public MiddleSwitch() {
 		if(Robot.gameData.charAt(0) == 'L') {
 			//addSequential(new DriveForwward(distance));
-			//addSequential(new Rotate(45));
+			//addSequential(new Rotate(-45));
 			//addSequential(new DriveForward(distance));
 			//addSequential(new PutCube));
 		}
 		if(Robot.gameData.charAt(0) == 'R') {
 			//addSequential(new DriveForwward(distance));
-			//addSequential(new Rotate(135));
+			//addSequential(new Rotate(45));
 			//addSequential(new DriveForward(distance));
 			//addSequential(new PutCube));
 		}
