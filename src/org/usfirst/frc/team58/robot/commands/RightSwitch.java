f066bb05c951ddcbd8129ed5487e47dcbf281a6d
Tyler Hansen <TyHansen00@gmail.com>
2018-01-24 17:42:35 -0500

    added template right and left auto
    added leftswitch and rightswitch boilerplate command groups

---
+++ src/org/usfirst/frc/team58/robot/commands/RightSwitch.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team58.robot.commands;
+
+import org.usfirst.frc.team58.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class RightSwitch extends CommandGroup {
+	public RightSwitch() {
+		if(Robot.gameData.charAt(0) == 'L') {
+			//addSequential(new DriveForwward(longerDistance));
+			//addSequential(new Rotate(-90));
+			//addSequential(new DriveForwward(farDistance));
+			//addSequential(new Rotate(-90));
+			//addSequential (new DriveForward(smallDistance));
+			//addSequential(new PutCube());
+		}
+		if(Robot.gameData.charAt(0) == 'R') {
+			//addSequential(new DriveForwward(distance));
+			//addSequential(new Rotate(-90));
+			//addSequential(new DriveForward(smallDistance));
+			//addSequential(new PutCube));
+		}
+	}
+}
