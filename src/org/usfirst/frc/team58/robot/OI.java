c12ee9b10fc4c3a081574c12b5c2f3aa8ffd2b81
Primary Code Laptop <riotcrewprogrammers@gmail.com>
2018-02-12 18:53:54 -0500

    random changes before switch

--- src/org/usfirst/frc/team58/robot/OI.java
+++ src/org/usfirst/frc/team58/robot/OI.java
@@ -65,8 +65,8 @@ public class OI {
 		ExchangeHeight.whenPressed(new PIDElevate(Dashboard.ExchangeHeight));
 		RestHeight.whenPressed(new PIDElevate(Dashboard.RestHeight));
 		Climb.whileHeld(new Climb());
-		RearElevateUP.whileHeld(new RunRearElevator(0.8));
-		RearElevateDOWN.whileHeld(new RunRearElevator(-0.8));
+		RearElevateUP.whileHeld(new RunRearElevator(0.1));
+		RearElevateDOWN.whileHeld(new RunRearElevator(-0.1));
 		SpitOutCube.whenPressed(new SpitCube());
 		
 	}
