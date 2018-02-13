9abf8894f8b602b5703a8f9e72dc3808fd709a88
Primary Code Laptop <riotcrewprogrammers@gmail.com>
2018-02-12 19:26:13 -0500

    working on appropriate delays

---
+++ src/org/usfirst/frc/team58/robot/commands/ResetWait.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team58.robot.commands;
+
+import org.usfirst.frc.team58.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ResetWait extends Command {
+	
+	long startTime;
+	double duration; 
+	
+	public ResetWait(double duration) {
+		requires(Robot.m_DriveTrain);
+		this.duration = duration;
+	}
+	
+	@Override
+	protected void initialize() {
+		startTime = System.currentTimeMillis();
+		Robot.m_DriveTrain.drive(0, 0, false);
+		Robot.m_DriveTrain.zeroEncoders();
+		Robot.m_DriveTrain.zeroNavx();
+	}
+	
+	@Override
+	protected void execute() {
+		Robot.m_DriveTrain.drive(0, 0, false);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// Checks to see if duration has elapsed since start time
+		if (System.currentTimeMillis() >= startTime + duration) {
+			return true;
+		} else {
+			System.out.println("Waiting");
+			return false;
+		}
+		
+	}
+	
+	@Override
+	protected void end() {
+		Robot.m_DriveTrain.drive(0, 0, false);
+	}
+	
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
--- src/org/usfirst/frc/team58/robot/commands/RightAngle.java
+++ src/org/usfirst/frc/team58/robot/commands/RightAngle.java
@@ -1,28 +1,15 @@
 package org.usfirst.frc.team58.robot.commands;
 
 import org.usfirst.frc.team58.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitForChildren;
 
 public class RightAngle extends CommandGroup {
 	public RightAngle() {
-		addSequential(new PIDdrive(3,0,0,36));
-		
+		addSequential(new PIDdrive(3,0,0,120));
+		addSequential(new ResetWait(250)); // Wait 1000ms
 		addSequential(new PIDRotate(0.03,0.006,0.07,90));
-	
-		addSequential(new PIDdrive(3,0,0,72));
-		//if(Robot.gameData.charAt(0) == 'L') {
-			//addSequential(new DriveForwward(distance));
-			//addSequential(new Rotate(-45));
-			//addSequential(new DriveForward(distance));
-			//addSequential(new PutCube));
-		//}
-		//if(Robot.gameData.charAt(0) == 'R') {
-			//addSequential(new DriveForwward(distance));
-			//addSequential(new Rotate(45));
-			//addSequential(new DriveForward(distance));
-			//addSequential(new PutCube));
-		//}
+		addSequential(new ResetWait(250)); // Wait 1000ms
+		addSequential(new PIDdrive(3,0,0,36));
 	}
 }
